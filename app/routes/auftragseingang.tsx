788d26eb1a839ec5ca1bdad3948a9de1e468e23c
Lunchdaily <info@lunchdaily.de>
2026-07-16 14:15:34 +0200

    Experimentelles Design der Eingangszentrale zurücknehmen

--- app/routes/auftragseingang.tsx
+++ app/routes/auftragseingang.tsx
@@ -2,7 +2,6 @@ import {
   useEffect, useState } from "react";
 import AppLayout from "../components/AppLayout";
 import auftragseingangStyles from "../styles/auftragseingang.css?url";
-import auftragseingangReferenceStyles from "../styles/auftragseingang-reference.css?url";
 import { Form, Link, redirect, useActionData, useFetcher, useLoaderData,
   useSearchParams,
 } from "react-router";
@@ -13,11 +12,7 @@ export function links() {
       rel: "stylesheet",
       href: auftragseingangStyles,
     },
-      {
-      rel: "stylesheet",
-      href: auftragseingangReferenceStyles,
-    },
-];
+  ];
 }
 
 const EMAIL_BUCKETS = [
